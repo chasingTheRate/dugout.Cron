446a5734083f73d46f367a26ae7ac353056566c6
Mark <mark.e.eaton@gmail.com>
2019-05-11 14:40:21 -0500

    updated

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ const CronJob = require('cron').CronJob;
 const moment = require('moment');
 const dugout = require('./services/dugoutService');
 
-console.log(`Starting Cron Jobs...`);
+console.log(`Starting Cron Jobs, time: ${moment().format('MMMM Do YYYY, h:mm:ss a')}`);
 
 //  Daily Cron
 
-new CronJob('0 0 3 * * *', () => {
+new CronJob('0 38 14 * * *', () => { // 0 0 3 * * *
   console.log(`Cron Fired - Time: ${moment().format('MMMM Do YYYY, h:mm:ss a')}`)
   dugout.getInitialBoxscores();
 }, null, true, 'America/Winnipeg');
 
 dugout.getInitialBoxscores();
-console.log(moment().format('LTS'));
