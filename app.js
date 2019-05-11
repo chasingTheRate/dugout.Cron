ff7d22d58c941dcc92e51bd07ca426a77f8e1e29
markeaton <markeaton@Marks-Mac-mini.local>
2019-05-11 14:28:39 -0500

    updated cron

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ console.log(`Starting Cron Jobs...`);
 //  Daily Cron
 
 new CronJob('0 0 3 * * *', () => {
-  console.log(`Cron Fired - Time: ${moment.format('MMMM Do YYYY, h:mm:ss a')}`)
+  console.log(`Cron Fired - Time: ${moment().format('MMMM Do YYYY, h:mm:ss a')}`)
   dugout.getInitialBoxscores();
 }, null, true, 'America/Winnipeg');
 
 dugout.getInitialBoxscores();
+console.log(moment().format('LTS'));
